b01ba5d6737e3b2989c4ec9f19bfa0fdf4955cd4
Branbados <brandonaltermatt@gmail.com>
2021-07-08 16:56:25 -0700

    added admin api path and product views

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,13 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
 
 class ProductViewSet(viewsets.ViewSet):
     # /api/products
     def list(self, request):
-        ...
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
     # /api/products
     def create(self, request):
-        ...
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     # /api/products/<str:id>
     def retrieve(self, request, pk=None):
