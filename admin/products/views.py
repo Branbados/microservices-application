aa22f81045a733175608b57c74799b6e5a4d1119
Branbados <brandonaltermatt@gmail.com>
2021-07-08 16:27:12 -0700

    added endpoints and views for products

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    # /api/products
+    def list(self, request):
+        ...
+
+    # /api/products
+    def create(self, request):
+        ...
+
+    # /api/products/<str:id>
+    def retrieve(self, request, pk=None):
+        ...
+
+    # /api/products/<str:id>
+    def update(self, request, pk=None):
+        ...
+
+    # /api/products/<str:id>
+    def destroy(self, request, pk=None):
+        ...
