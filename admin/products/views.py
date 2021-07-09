69cfda05e0438b3aa45aeed257698f50ea219810
Branbados <brandonaltermatt@gmail.com>
2021-07-08 17:16:30 -0700

    added user view and path

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets, status
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework import serializers, viewsets, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
+import random
+
+from .models import Product, User
+from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ViewSet):
     # /api/products
@@ -19,12 +22,28 @@ class ProductViewSet(viewsets.ViewSet):
 
     # /api/products/<str:id>
     def retrieve(self, request, pk=None):
-        ...
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
 
     # /api/products/<str:id>
     def update(self, request, pk=None):
-        ...
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)        
 
     # /api/products/<str:id>
     def destroy(self, request, pk=None):
-        ...
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id
+        })
